f8cc5e2b19418e15c69faae93f530d2eb429c0cb
Emily Regolão <emregolao@gmail.com>
2021-10-18 09:49:25 -0300

    style(component/menu): stylizes the menu [mobile and desktop]

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,19 +1,35 @@
-import { MenuContainer } from "./style"
+import { MenuContainer, LogoutContainer, IconsContainer } from "./style"
 import { MdGroups } from "react-icons/md"
 import { BiLogOutCircle } from "react-icons/bi"
 import { FaHeartbeat } from "react-icons/fa"
 import Button from "../Button"
+import Logo from "../../assets/img/logo.png"
 
-const Menu = () => {
-  const handleTeste = () => {
-    alert("teste")
-  }
+const Menu = ({ handleHabits, handleGroups, handleAdd, handleLogout }) => {
   return (
     <MenuContainer>
-      <FaHeartbeat onClick={handleTeste} />
-      <MdGroups onClick={handleTeste} />
-      <Button text='+' smaller />
-      <BiLogOutCircle onClick={handleTeste} />
+      <img alt='logo' src={Logo} />
+
+      <IconsContainer>
+        <span>
+          <FaHeartbeat onClick={handleHabits} />
+          <p>Hábitos</p>
+        </span>
+
+        <span>
+          <MdGroups onClick={handleGroups} />
+          <p>Grupos</p>
+        </span>
+      </IconsContainer>
+
+      <Button text='+' smaller onClick={handleAdd} />
+
+      <LogoutContainer>
+        <span>
+          <BiLogOutCircle onClick={handleLogout} />
+          <p>Sair</p>
+        </span>
+      </LogoutContainer>
     </MenuContainer>
   )
 }
