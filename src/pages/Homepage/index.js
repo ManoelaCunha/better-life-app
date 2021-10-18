4e5063f80da74645d977bd391f7b6ac9023cb21a
Emily Regolão <85745693+emilyregolao@users.noreply.github.com>
2021-10-18 10:43:41 -0300

    Revert "Developer"

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,31 +1,9 @@
-import { Container, Content, ImageHome } from "./style";
-import Logo from "../../assets/img/logo.png";
-import ImgHome from "../../assets/img/Vector.png";
-import Button from "../../components/Button";
-import { useHistory } from "react-router";
-// A prop authenticated será usada para autenticar para qe o ussuário não vá direto pra o core da aplicação
-const Homepage = ({ authenticated }) => {
-  const history = useHistory();
-  const handleNavigation = (path) => {
-    // navegação
-  }
-  if (authenticated) {
-    // Lógica para autenticar
-  }
+const Homepage = () => {
   return (
-    <Container>
-      <Content>
-        <img src={Logo} alt="Logo Better Life" />
-        <span>Melhore sua vida com hábitos saudáveis</span>
-        <div>
-          <Button text="Cadastre"></Button>
-          <Button text="Login">Login</Button>
-        </div>
-      </Content>
+    <div>
+      <h1>Homepage</h1>
+    </div>
+  )
+}
 
-      <ImageHome src={ImgHome} alt="Imagem tela inicial" />
-    </Container>
-  );
-};
-
-export default Homepage;
+export default Homepage
