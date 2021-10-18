1c290084819d1d8e30b0d62e140494157677c67a
Manoela Cunha <manoela_fg@hotmail.com>
2021-10-18 13:30:19 -0300

    feat(providers): add handleCreateGroup in Groups providers

--- src/providers/Groups/index.js
+++ src/providers/Groups/index.js
@@ -1,5 +1,7 @@
 import { createContext, useState, useEffect } from "react";
 
+import toast from "react-hot-toast";
+
 import api from "../../services/api";
 
 export const GroupsContext = createContext();
@@ -7,6 +9,10 @@ export const GroupsContext = createContext();
 export const GroupsProvider = ({ children }) => {
   const [groups, setGroups] = useState([]);
   const [next, setNext] = useState(1);
+
+  const [token] = useState(
+    JSON.parse(window.localStorage.getItem("@BetterLife:user")) || ""
+  );
 
   const getGroups = () => {
     api
@@ -22,8 +28,21 @@ export const GroupsProvider = ({ children }) => {
     getGroups();
   }, [next]);
 
+  const handleCreateGroup = (data) => {
+    api
+      .post("groups/", data, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        console.log(response.data);
+        setGroups([...groups, response.data]);
+        toast("Grupo criado com sucesso!");
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <GroupsContext.Provider value={{ groups, getGroups }}>
+    <GroupsContext.Provider value={{ groups, handleCreateGroup }}>
       {children}
     </GroupsContext.Provider>
   );
