6cc49d0ceef5030ca51a8509652394e75cf7ee58
Emily Regolão <emregolao@gmail.com>
2021-10-19 13:32:17 -0300

    perf(modal/habitos): implements the form to add new habits on dashboard

--- src/providers/Groups/index.js
+++ src/providers/Groups/index.js
@@ -3,8 +3,7 @@ import { createContext, useState, useEffect } from "react";
 import toast from "react-hot-toast";
 
 import api from "../../services/api";
-import axios from 'axios';
-
+import axios from "axios";
 
 export const GroupsContext = createContext();
 
@@ -13,9 +12,7 @@ export const GroupsProvider = ({ children }) => {
   const [next, setNext] = useState(1);
   const [subscribedGroups, setSubscribedGroups] = useState([]);
 
-  const [token] = useState(
-    JSON.parse(localStorage.getItem("@BetterLife:token")) || ""
-  );
+  const [token] = useState(JSON.parse(localStorage.getItem("@BetterLife:token")) || "");
 
   const getGroups = () => {
     api
@@ -46,36 +43,48 @@ export const GroupsProvider = ({ children }) => {
 
   const subscribeGroup = (id) => {
     const token = JSON.parse(localStorage.getItem("@BetterLife:token"));
-    axios.post(`https://kenzie-habits.herokuapp.com/groups/${id}/subscribe/`, {}, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
+    axios
+      .post(
+        `https://kenzie-habits.herokuapp.com/groups/${id}/subscribe/`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${token}` },
+        }
+      )
       .then(() => {
         toast.success("Inscrição feita com sucesso!");
         setSubscribedGroups([...subscribedGroups, id]);
-      }).catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
   };
 
   const unsubscribeGroup = (id) => {
     const token = JSON.parse(localStorage.getItem("@BetterLife:token"));
-    api.delete(`groups/${id}/unsubscribe/`, {
-      headers: {
-        Authorization: `Bearer ${token}`
-      },
-    }).then(() => {
-      toast.success("Removido do grupo com sucesso!");
-      const newSubGroups = subscribedGroups.filter((groupId) => groupId !== id);
-      setSubscribedGroups(newSubGroups);
-    }).catch((err) => console.log(err));
+    api
+      .delete(`groups/${id}/unsubscribe/`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => {
+        toast.success("Removido do grupo com sucesso!");
+        const newSubGroups = subscribedGroups.filter((groupId) => groupId !== id);
+        setSubscribedGroups(newSubGroups);
+      })
+      .catch((err) => console.log(err));
   };
 
   const getSubscribedGroups = () => {
     const token = JSON.parse(localStorage.getItem("@BetterLife:token"));
-    api.get('groups/subscriptions/', {
-      headers: { Authorization: `Bearer ${token}` },
-    }).then((resp) => {
-      const newSubGroups = resp.data.map((group) => group.id);
-      setSubscribedGroups(newSubGroups);
-    }).catch(err => console.log(err.message));
+    api
+      .get("groups/subscriptions/", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((resp) => {
+        const newSubGroups = resp.data.map((group) => group.id);
+        setSubscribedGroups(newSubGroups);
+      })
+      .catch((err) => console.log(err.message));
   };
 
   return (
