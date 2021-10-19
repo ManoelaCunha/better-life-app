1fba01698beb14ef60fb9db3ee32704915d0355e
Emily Regolão <emregolao@gmail.com>
2021-10-19 10:07:20 -0300

    feat(component/modal): creates a modal component

---
+++ src/components/Modal/style.js
@@ -0,0 +1,19 @@
+import { styled } from "styled-components";
+
+export const CloseModal = styled.button`
+  background-color: transparent;
+  border: none;
+  color: #314c88;
+  font-size: 26px;
+  position: absolute;
+  right: 20px;
+  top: 15px;
+`;
+
+export const ModalContent = styled.div`
+  padding: 30px 10px;
+  h2 {
+    margin-bottom: 20px;
+    color: #314c88;
+  }
+`;
