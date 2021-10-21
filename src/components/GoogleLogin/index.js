15536006891504f7ccc4de23a8c68dce59393fe5
Emily Regolão <emregolao@gmail.com>
2021-10-21 12:16:26 -0300

    feat(login/signup/google): implements the signup and login with google

--- src/components/GoogleLogin/index.js
+++ src/components/GoogleLogin/index.js
@@ -1,15 +1,34 @@
 import { GoogleLogin } from "react-google-login";
 import { GoogleHelper } from "./helpers";
+import { useLocation } from "react-router";
 
-const LoginGoogle = () => {
-  const handleLogin = () => {};
+const LoginGoogle = ({ handleGoogle }) => {
+  const location = useLocation();
+
+  const handleLogin = (response) => {
+    const cadastro = {
+      username: response.profileObj.givenName,
+      email: response.profileObj.email,
+      password: response.profileObj.googleId,
+    };
+    const login = {
+      username: response.profileObj.givenName,
+      password: response.profileObj.googleId,
+    };
+
+    if (location === "/signup") {
+      handleGoogle(cadastro);
+    } else {
+      handleGoogle(login);
+    }
+  };
 
   const { client_id } = GoogleHelper.web;
 
   return (
     <GoogleLogin
       clientId={client_id}
-      buttonText='Login with Google'
+      buttonText='Continuar com Google'
       onSuccess={handleLogin}
       onFailure={handleLogin}
       cookiePolicy={"single_host_origin"}
